aa36c0bc01f1adc4ebba8c7620b7079ff719ffba
mbronshteyn <michael.bronshteyn@gmail.com>
2023-06-12 09:57:17 -0700

    use named parameter instead of positional

--- src/main/java/com/mbronshteyn/usermanagement/repository/ThemeRepository.java
+++ src/main/java/com/mbronshteyn/usermanagement/repository/ThemeRepository.java
@@ -3,10 +3,11 @@ package com.mbronshteyn.usermanagement.repository;
 import com.mbronshteyn.usermanagement.entity.ThemeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ThemeRepository extends JpaRepository<ThemeEntity, Long> {
-    @Query(value = "select * from themes th where th.name = ?1", nativeQuery = true)
-    ThemeEntity findByName(String name);
+    @Query(value = "select * from themes th where th.name = :name", nativeQuery = true)
+    ThemeEntity findByName(@Param("name") String name);
 }
