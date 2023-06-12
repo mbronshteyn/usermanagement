4d38f0db802185598c0dc89f980bba450d9e6d82
mbronshteyn <michael.bronshteyn@gmail.com>
2023-06-12 09:42:29 -0700

    JPA Repository extends Paging and Sorting repository

--- src/main/java/com/mbronshteyn/usermanagement/repository/UserRepository.java
+++ src/main/java/com/mbronshteyn/usermanagement/repository/UserRepository.java
@@ -2,14 +2,13 @@ package com.mbronshteyn.usermanagement.repository;
 
 import com.mbronshteyn.usermanagement.entity.UserEntity;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
-
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByBatchNumber(String batchNumber);
 
     int deleteDistinctByBatchNumber(String batchNumber);
